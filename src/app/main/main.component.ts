e5d777963d9af465905112cc439d87e0cd7bafb1
Randyglobal <randyitoe2255@gmail.com>
2023-07-01 08:12:41 -0700

    Working on editing

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { StoreUerService } from '../service/store/store-uer.service';
 import { ITask } from '../interface/task.interface';
 import { Firestore, addDoc, collection, deleteDoc, doc, getDocs } from '@angular/fire/firestore';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -17,6 +17,8 @@ export class MainComponent implements OnInit{
 
   // for the forms
 taskForm: FormGroup
+// in the view child we pass the local reference variable
+// @ViewChild('addForm') form: NgForm;
 
   taskId = new Date().getTime().toString()
   constructor(private store: StoreUerService, public firestore: Firestore, private formGroup: FormBuilder){
@@ -84,8 +86,7 @@ taskForm: FormGroup
    }
    pop(){
     this.Popup = !this.Popup
-  //  alert("ID"+ this.taskId)
-    
+  //  alert("ID"+ this.taskId)  
    }
 
   // gettData(id: string){
@@ -134,6 +135,25 @@ taskForm: FormGroup
        alert(err)
      })
   }
-
+// edit
+  editTask(id: string){
+    this.Popup = !this.Popup
+    // get Product based n its id
+   let currentTask =  this.data.find((task: { id: any; }) => {
+     return task.id ===id
+    })
+    console.log(currentTask);
+    // we use the setValue method to set the value for the formControls, we pass an opject an
+    // in the object we specify the values  assigned to the inputs
+//  this.form?.setValue({
+//       taskName: 'bread',
+//       status: '',
+//       level: '',
+//       difficulty: '',
+//       startDate: '',
+//       dueDate: ''
+//     });
+   
+  }
  
 }
